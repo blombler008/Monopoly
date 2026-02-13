4ba15fd7b9df8877bfb7effe1b4fd5b1f307ac60
blombler008 <37677225+blombler008@users.noreply.github.com>
2026-02-13 20:45:30 +0100

    Refactor submodule paths and update references for Monopoly_GUI and rfid; adjust platformio.ini library directories and enhance GUI initialization in lv_start_gui.cpp

--- src/gui/lv_start_gui.cpp
+++ src/gui/lv_start_gui.cpp
@@ -1,5 +1,5 @@
 #include <components/lv_gui_containers.hpp> 
-
+#include <Monopoly.h>
 lv_obj_t* textInput_start_gui; 
 
 static void text_input_event_cb(lv_event_t * event) {
@@ -28,8 +28,8 @@ void lv_start_loop(void) {
 // GUI Creation
 void lv_create_start_gui_async(void*) { 
     // Create a text label aligned center on top ("Monopoly Banking")
-    // Monopoly_init("/sd/");
-    // lv_screen_load(welcome);
+    Monopoly_init("/sd/");
+    lv_screen_load(welcome);
     // lv_obj_t* src = lv_obj_assert_null(lv_screen_active());
     // lv_create_title_underline(src, "Monopoly Banking ₩");
      
